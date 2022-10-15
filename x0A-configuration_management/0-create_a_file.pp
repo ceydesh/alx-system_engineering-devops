f7baf51d488ef1631d53991166174657e7a1d663
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-15 04:37:35 +0300

    Configuration management

--- x0A-configuration_management/0-create_a_file.pp
+++ x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in the /tmp directory
-file { '/tmp/school':
-  ensure  => 'file',
+# Creatring a file in /tmp
+file {'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/school',
 }
