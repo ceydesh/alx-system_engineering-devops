e9cd159f346d9a76874cae4d32734350195f7339
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-11-15 06:04:44 +0300

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""DataAPI module"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-if __name__ == '__main__':
-    api_url = 'https://jsonplaceholder.typicode.com'
-    employee_id = sys.argv[1]
-    r1 = requests.get('{}/users/{}'.format(api_url, employee_id))
-    employee_name = r1.json().get('name')
-    r2 = requests.get('{}/todos?userId={}'.format(api_url, employee_id))
-    responses = r2.json()
-    done_tasks = [t for t in responses if t.get('completed')]
-    print('Employee {} is done with tasks({}/{}):'.
-          format(employee_name, len(done_tasks), len(responses)))
-    for t in done_tasks:
-        print('\t {}'.format(t.get('title')))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
